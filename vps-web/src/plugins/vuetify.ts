8c16308a422e13bbf78ec48e6dc97242eeeb6619
shc <15611217599@163.com>
2025-08-17 11:56:11 +0800

    稳定版007

--- vps-web/src/plugins/vuetify.ts
+++ vps-web/src/plugins/vuetify.ts
@@ -3,7 +3,6 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
 
 // 默认主题配色
 const defaultColors = {
